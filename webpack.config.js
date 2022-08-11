d0290c777b2b83afcd53218c0878fc355d98d47d
Mayra234 <alejandradiavanera@gmail.com>
2022-08-11 00:22:57 -0500

    Creación e intalación de los demás documentos del proyecto

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,34 @@
-module.exports = {};
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.jsx",
+  mode: "development",
+  devtool: "source-map",
+  resolve: { extensions: [".js", ".jsx"] },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+        type: "asset",
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+    devServer: {
+      open: true,
+      historyApiFallback: true,
+    },
+    plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+  },
+};
