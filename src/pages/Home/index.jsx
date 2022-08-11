04ee0f147201a5a90532af2bb5e56ab302c6a490
Mayra234 <alejandradiavanera@gmail.com>
2022-08-11 13:43:58 -0500

    Se crea componente de pagina home

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { PlayerMarker } from "../../components/PlayerMarker";
+import { Card } from "../../components/Card";
+import { faHand } from "@fortawesome/free-regular-svg-icons";
+import { faHandScissors } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export const Home = () => {
+  return (
+    <>
+      <div className="player-content">
+        <PlayerMarker name="Tú" numberRound="1" />
+        <Card>
+          <FontAwesomeIcon
+            style={{ width: "100px", height: "100px" }}
+            icon={faHand}
+          />
+        </Card>
+      </div>
+      <div>
+        <PlayerMarker name="vs" />
+      </div>
+      <div className="opponent-content">
+        <PlayerMarker name="pc" numberRound="2" />
+        <Card>
+          <FontAwesomeIcon
+            style={{ width: "100px", height: "100px" }}
+            icon={faHandScissors}
+          />
+        </Card>
+      </div>
+    </>
+  );
+};
