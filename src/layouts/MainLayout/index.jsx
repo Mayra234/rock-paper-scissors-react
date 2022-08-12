dd2b2a424ade8a1d4afaa815fdda27fadf9b78ff
Mayra234 <alejandradiavanera@gmail.com>
2022-08-12 17:56:32 -0500

    Se organiza el código y se hacen mejoras en MainLayout

--- src/layouts/MainLayout/index.jsx
+++ src/layouts/MainLayout/index.jsx
@@ -1,38 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./index.css";
-import { Button } from "../../components/Button";
 import { Sidebar } from "../../components/Sidebar";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHand } from "@fortawesome/free-regular-svg-icons";
-import { faHandBackFist } from "@fortawesome/free-regular-svg-icons";
-import { faHandScissors } from "@fortawesome/free-regular-svg-icons";
-import { Board } from "../../components/Board";
+import { faHand } from "@fortawesome/free-solid-svg-icons";
+import { faHandBackFist } from "@fortawesome/free-solid-svg-icons";
+import { faHandScissors } from "@fortawesome/free-solid-svg-icons";
+import { ButtonIcon } from "../../components/ButtonIcon";
 
 export const MainLayout = ({ children = "" }) => {
   return (
     <div className="content-layout">
       <Sidebar>
-        <Button>
-          <FontAwesomeIcon
-            style={{ width: "40px", height: "40px" }}
-            icon={faHand}
-          />
-        </Button>
-        <Button>
-          <FontAwesomeIcon
-            style={{ width: "40px", height: "40px" }}
-            icon={faHandBackFist}
-          />
-        </Button>
-        <Button>
-          <FontAwesomeIcon
-            style={{ width: "40px", height: "40px" }}
-            icon={faHandScissors}
-          />
-        </Button>
+        <ButtonIcon icon={faHand} />
+        <ButtonIcon icon={faHandBackFist} />
+        <ButtonIcon icon={faHandScissors} />
       </Sidebar>
       <div className="separator"></div>
-      <Board>{children}</Board>
+      {children}
     </div>
   );
 };
--- src/layouts/MainLayout/index.stories.jsx
+++ src/layouts/MainLayout/index.stories.jsx
@@ -1,39 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { PlayerMarker } from "../../components/PlayerMarker";
 import { Card } from "../../components/Card";
-import { faHand } from "@fortawesome/free-regular-svg-icons";
+import { faHand, faHandBackFist } from "@fortawesome/free-regular-svg-icons";
 import { faHandScissors } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { MainLayout } from "./index";
+import { Board } from "../../components/Board";
 
 export default {
   title: "Layouts/MainLayout",
 };
 
 export const Overview = () => {
+  const [choicePlayer, setChoicePlayer] = useState(0);
+
   return (
     <MainLayout>
-      <div className="player-content">
-        <PlayerMarker name="Tú" numberRound="1" />
-        <Card>
-          <FontAwesomeIcon
-            style={{ width: "100px", height: "100px" }}
-            icon={faHand}
-          />
-        </Card>
-      </div>
-      <div>
-        <PlayerMarker name="vs" />
-      </div>
-      <div className="opponent-content">
-        <PlayerMarker name="pc" numberRound="2" />
-        <Card>
-          <FontAwesomeIcon
-            style={{ width: "100px", height: "100px" }}
-            icon={faHandScissors}
-          />
-        </Card>
-      </div>
+      <Board>
+        <div className="player-content">
+          <PlayerMarker name="Tú" numberRound="1" />
+          <Card>
+            <FontAwesomeIcon
+              className="icon"
+              style={{ width: "250px", height: "250px" }}
+              icon={faHand}
+            />
+          </Card>
+        </div>
+        <div className="vs">
+          <p>vs</p>
+        </div>
+        <div className="opponent-content">
+          <PlayerMarker name="pc" numberRound="2" />
+          <Card>
+            <FontAwesomeIcon
+              className="icon"
+              style={{ width: "250px", height: "250px" }}
+              icon={faHandBackFist}
+            />
+          </Card>
+        </div>
+      </Board>
     </MainLayout>
   );
 };
